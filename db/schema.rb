05558aaefd19a93033dc63e815286280b9d3909d
Evgeniy-ES <ees86@yandex.ru>
2022-02-17 11:55:47 +0300

    Attempt 1 Create tests and model Answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_194033) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_17_081654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "text"
+    t.bigint "question_id"
+    t.boolean "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -21,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_194033) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
---
+++ spec/factories/answers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :answer do
+    text { "MyText" }
+    correct { false }
+  end
+end
